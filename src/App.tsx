1f821899b5c2547a859c6b57e9f8bb78019232df
taha <tahaesii.dev@gmail.com>
2025-01-19 00:38:32 +0330

    sel

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { DarkModeToggle as Control } from "./controls/darkModeRadioBtn/DarkModeToggle";
+
+function App() {
+  return (
+    <>
+      <Control />
+    </>
+  );
+}
+
+export default App;
